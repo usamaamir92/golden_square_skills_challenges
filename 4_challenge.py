26a6e1ee72aba675f29059e66b97a50309a43705
Usama <usama_amir92@hotmail.com>
2023-06-13 17:20:04 +0100

    4_challenge

--- 4_challenge.py
+++ 4_challenge.py
@@ -5,9 +5,7 @@ def get_most_common_letter(text):
         counter[char] = counter.get(char, 0) + 1
     # print('counter.items',list(counter.items()))
     # counter.items = [('t', 3), ('h', 3), ('e', 4), (' ', 8), ('r', 4), ('o', 7), ('f', 4), (',', 2), ('i', 2), ('s', 1), ('n', 1), ('!', 1)]
-    letter = sorted(list(counter.items()), key=lambda item: item[1], reverse = True)[0]
-    most_common_letter = list(letter.items())[0]
-    print(letter)
+    letter = sorted(list(counter.items()), key=lambda item: item[1], reverse = True)[0][0]
     return letter
 
 
